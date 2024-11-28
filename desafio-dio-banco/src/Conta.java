23fdd1ab66c84e1321060e80b3eef865f4eae8d1
Simone <simonecabral.br@gmail.com>
2024-11-28 09:53:52 -0300

    Add de exercícios de revisão

---
+++ desafio-dio-banco/src/Conta.java
@@ -0,0 +1,42 @@
+public abstract class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int conta;
+    protected double saldo;
+
+    public Conta(){
+        agencia = AGENCIA_PADRAO;
+        conta = SEQUENCIAL++;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    @Override
+    public void sacar(double valor){
+
+    }
+
+    @Override
+    public void depositar(double valor){
+
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino){
+
+    }
+
+}
